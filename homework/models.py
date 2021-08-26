363a0f2636447dd49e3474492a60002ef61c9259
Elijah Lopez <elijah.kane.1972@gmail.com>
2021-08-25 20:27:44 -1000

    Model Change

--- homework/models.py
+++ homework/models.py
@@ -11,7 +11,7 @@ class HomeworkAssignment(models.Model):
     name = models.CharField(max_length=120, null=True)
     description = models.TextField(null=True, blank=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-    due_date = models.DateField(default=timezone.now)
+    due_date = models.DateField(default=datetime.datetime.now())
     due_time = models.TimeField(default='09:00:00')
     link = models.URLField(null=True, blank=True)
     completed = models.BooleanField(default=False)
