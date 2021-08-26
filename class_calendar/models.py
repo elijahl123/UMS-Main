a807731673bf945eff757e16226981e1c669bc70
Elijah Lopez <elijah.kane.1972@gmail.com>
2021-08-25 20:33:57 -1000

    Remove Migrations

--- class_calendar/models.py
+++ class_calendar/models.py
@@ -7,7 +7,7 @@ from users.models import Account
 
 
 class CalendarEvent(models.Model):
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(null=True)
     user = models.ForeignKey(Account, null=True, on_delete=models.CASCADE)
     time = models.TimeField(help_text='Leave Blank if Event is All Day', null=True, blank=True)
     title = models.CharField(max_length=120, null=True)
