ff9c4c231ea2da5d42b5acc1c469a8c9cd558df0
Elijah Lopez <elijah.kane.1972@gmail.com>
2021-09-08 23:56:01 -1000

    Connect Google Calendar Attempt 3

--- class_calendar/views.py
+++ class_calendar/views.py
@@ -1,10 +1,13 @@
 import calendar
 import json
+import os
 from datetime import date
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -17,6 +20,25 @@ from homework.models import HomeworkAssignment
 from school.models import School
 
 context = {'schools': School.objects.all()}
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+credentials = {
+    "web": {
+        "client_id": "9730399367-hikc9cjjco8kpn750po400dmo4ke3uhu.apps.googleusercontent.com",
+        "project_id": "untitled-management-software",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "OunQv9kY3lmfXdaRGBOuo6JH",
+        "redirect_uris": ["http://localhost/accounts/google/login/callback/",
+                          "https://client.untitledmanagementsoftware.com/accounts/google/login/callback/",
+                          "http://localhost/calendar/save-google-credentials/",
+                          "https://client.untitledmanagementsoftware.com/calendar/save-google-credentials/",
+                          "http://localhost/",
+                          "http://client.untitledmanagementsoftware.com/"]
+    }
+}
 
 
 @login_required
@@ -101,13 +123,11 @@ def delete_calendar_event(request, id):
 def connect_google_calendar(request):
     context['account'] = request.user
 
-    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if CalendarToken.objects.filter(user=request.user).exists():
+    if CalendarToken.objects.filter(user=request.user, token__isnull=False).exists():
         creds = Credentials.from_authorized_user_info(json.loads(CalendarToken.objects.get(user=request.user).token),
                                                       SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -115,25 +135,25 @@ def connect_google_calendar(request):
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            credentials = {"web": {
-                "client_id": "9730399367-hikc9cjjco8kpn750po400dmo4ke3uhu.apps.googleusercontent.com",
-                "project_id": "untitled-management-software",
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_secret": "OunQv9kY3lmfXdaRGBOuo6JH",
-                "redirect_uris": ["http://localhost/accounts/google/login/callback/",
-                                  "https://client.untitledmanagementsoftware.com/accounts/google/login/callback/",
-                                  "http://localhost/calendar/connect-google/",
-                                  "https://client.untitledmanagementsoftware.com/calendar/connect-google/",
-                                  "http://localhost/",
-                                  "http://client.untitledmanagementsoftware.com/"]
-            }
-            }
             flow = InstalledAppFlow.from_client_config(
                 credentials, SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        CalendarToken.objects.update_or_create(user=request.user, token=creds.to_json())
+            flow.redirect_uri = request.build_absolute_uri(reverse('save_google_credentials'))
+            authorization_url, state = flow.authorization_url(
+                # Enable offline access so that you can refresh an access token without
+                # re-prompting the user for permission. Recommended for web server apps.
+                access_type='offline',
+                login_hint=request.user.email,
+                # Enable incremental authorization. Recommended as a best practice.
+                include_granted_scopes='true')
+            # Save the credentials for the next run
+            return HttpResponseRedirect(authorization_url)
 
+    return redirect('save_google_credentials')
+
+
+@login_required()
+def save_google_credentials(request):
+    flow = InstalledAppFlow.from_client_config(credentials, SCOPES, state=request.GET.get('state'))
+    token = flow.fetch_token(authorization_response=reverse('save_google_credentials'))
+    CalendarToken.objects.update_or_create(user=request.user, token=token)
     return redirect('calendar')
