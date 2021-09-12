b9ecdf5652ecefe948e2a241304087b7e3eee9b4
Elijah Lopez <elijah.kane.1972@gmail.com>
2021-09-11 16:09:21 -1000

    Is Due Soon JS

--- static/js/main.js
+++ static/js/main.js
@@ -14,17 +14,38 @@ window.addEventListener("load", function () {
     $.ajax({
         url: '/api/assignments/view/',
         success: function (data) {
-            const d = new Date()
+            const today = new Date()
+            const tomorrow = new Date(today)
+            tomorrow.setDate(tomorrow.getDate() + 1)
+            const isSoon = (d) => {
+                return (d.getDate() === today.getDate() &&
+                    d.getMonth() === today.getMonth() &&
+                    d.getFullYear() === today.getFullYear()) ||
+                    (d.getDate() === tomorrow.getDate() &&
+                    d.getMonth() === tomorrow.getMonth() &&
+                    d.getFullYear() === tomorrow.getFullYear())
+            }
             let late_assignments = 0
+            let due_soon = 0
             $.each(data, function (index, element) {
                 const due = new Date(element.due_date + 'T' + element.due_time)
-                if (!element.completed && due < d) {
+                if (!element.completed && due < today) {
                     late_assignments += 1
                 }
+
+                if (!element.completed && !(due < today) && isSoon(due)) {
+                    due_soon += 1
+                }
+
             })
             if (late_assignments) {
                 $('#homework-nav-link').append(`
                     <span class="badge bg-danger ms-2">${late_assignments} Late</span>
+                `)
+            }
+            if (due_soon) {
+                $('#homework-nav-link').append(`
+                    <span class="badge bg-warning ms-2">${due_soon} Due Soon</span>
                 `)
             }
         }
