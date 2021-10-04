6984694c5598c835ed841da7871aa0ef8ea909c4
Elijah Lopez <elijah.kane.1972@gmail.com>
2021-10-04 09:25:05 -1000

    Daily Notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
@@ -36,7 +38,7 @@ def add_notes(request, course_id):
     if not course.user == request.user:
         return redirect('notes_home')
 
-    obj = Note.objects.create(course=course, user=request.user)
+    obj = Note.objects.create(course=course, user=request.user, title=f'Notes for {datetime.date.today().isoformat()}') if request.GET['daily-notes'] else Note.objects.create(course=course, user=request.user)
 
     return redirect('notes_view_notes', course_id, obj.id)
 
