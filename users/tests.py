4d6e7754842eabb10e7851063f3162e8742e20a8
Elijah Lopez <Elijah.kane.1972@gmail.com>
2021-12-02 23:46:41 -1000

    Test Failed

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from users.models import Account
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        Account.objects.create_user(
+            email='elijah.kane.1972@gmail.com',
+            username='elijahl123',
+            first_name='Elijah',
+            last_name='Lopez',
+            password='password'
+        )
+
+    def test_account_subscription(self):
+        account = Account.objects.get(email='elijah.kane.1972@gmail.com')
+        self.assertTrue(account.subscription())
