25b8b765ba3b393631920832f247b972f4fa0b21
Elijah Lopez <Elijah.kane.1972@gmail.com>
2022-01-19 20:48:33 -0800

    Revert Back

--- homework/notifications.py
+++ homework/notifications.py
@@ -1,6 +1,7 @@
 import datetime
 
 import pytz
+from django.db.models import Count
 from django.template.loader import render_to_string
 
 from base.decorators import register_class
@@ -11,37 +12,40 @@ from users.models import Account
 
 @register_class()
 class HomeworkDueInSixHoursNotifications(NotificationsConfig):
-    user_constraints = {
-        'homework_notifications': True,
-        'timezone__isnull': False
-    }
     _in_hours = 6
 
-    def get_notifications(self, user):
-        now = datetime.datetime.now(tz=pytz.timezone(user.timezone)) + datetime.timedelta(hours=self._in_hours)
-        assignments = HomeworkAssignment.objects.filter(
-            course__user=user,
-            due_date=now.date(),
-            due_time__hour=now.hour,
-            due_time__minute=now.minute
-        )
-        if assignments.exists():
-            subject = f'{assignments.count()} Homework Assignment{"s" if assignments.count() > 1 else ""} Due '
-            if self._in_hours:
-                subject += f'In {self._in_hours} Hours'
-            else:
-                subject += 'Now'
-            self.current_notifications.append(Notification(
-                subject=subject,
-                message=render_to_string(
-                    'email/homework_assignments.txt',
-                    context={
-                        'subject': subject,
-                        'assignments': assignments
-                    }
-                ),
-                recipient=user
-            ))
+    def get_notifications(self):
+        assignments_users = HomeworkAssignment.objects.filter(
+            completed=False,
+            course__user__homework_notifications=True,
+            course__user__timezone__isnull=False
+        ).values('course__user').annotate(assignment_count=Count('course__user')).order_by()
+        for user in assignments_users:
+            account = Account.objects.get(id=user['course__user'])
+            now = datetime.datetime.now(tz=pytz.timezone(account.timezone)) + datetime.timedelta(hours=self._in_hours)
+            assignments = HomeworkAssignment.objects.filter(
+                course__user=account,
+                due_date=now.date(),
+                due_time__hour=now.hour,
+                due_time__minute=now.minute
+            )
+            if assignments.exists():
+                subject = f'{assignments.count()} Homework Assignment{"s" if assignments.count() > 1 else ""} Due '
+                if self._in_hours:
+                    subject += f'In {self._in_hours} Hours'
+                else:
+                    subject += 'Now'
+                self.current_notifications.append(Notification(
+                    subject=subject,
+                    message=render_to_string(
+                        'email/homework_assignments.txt',
+                        context={
+                            'subject': subject,
+                            'assignments': assignments
+                        }
+                    ),
+                    recipient=account
+                ))
 
 
 @register_class()
