581d7561c1439aab2a19ea21b282ec0f58e31190
Elijah Lopez <elijah.kane.1972@gmail.com>
2022-09-02 16:15:26 -1000

    Api Mixin

--- base/models.py
+++ base/models.py
@@ -1,22 +1,23 @@
+import uuid
+
 from django.db import models
 
 reminder_choices = [
-        (-1, "None"),
-        (0, "At time of event"),
-        (5, "5 minutes Before"),
-        (10, "10 minutes Before"),
-        (15, "15 minutes Before"),
-        (30, "30 minutes Before"),
-        (60, "1 hour Before"),
-        (120, "2 hours Before"),
-        (1440, "1 day Before"),
-        (2880, "2 days Before"),
-        (10080, "1 week Before")
-    ]
+    (-1, "None"),
+    (0, "At time of event"),
+    (5, "5 minutes Before"),
+    (10, "10 minutes Before"),
+    (15, "15 minutes Before"),
+    (30, "30 minutes Before"),
+    (60, "1 hour Before"),
+    (120, "2 hours Before"),
+    (1440, "1 day Before"),
+    (2880, "2 days Before"),
+    (10080, "1 week Before")
+]
 
 
 class ReminderMixin(models.Model):
-
     alert = models.IntegerField(choices=reminder_choices, default=0)
     second_alert = models.IntegerField(choices=reminder_choices, default=-1)
 
@@ -25,6 +26,13 @@ class ReminderMixin(models.Model):
 
     def get_second_alert(self):
         return dict(reminder_choices).get(self.second_alert, f"{self.second_alert} minutes before")
+
+    class Meta:
+        abstract = True
+
+
+class ApiMixin(models.Model):
+    api_id = models.UUIDField(default=uuid.uuid4, editable=False)
 
     class Meta:
         abstract = True
