b072cf755894a8dfbe1c592dcda21ba19db7db98
Elijah Lopez <Elijah.kane.1972@gmail.com>
2022-09-02 17:19:35 -1000

    Empty Values

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,1 @@
-from abc import ABC
-
-from django.forms import model_to_dict
-from rest_framework import serializers
-
-from courses.models import Course, CourseTime
-from homework.models import HomeworkAssignment
-
 
