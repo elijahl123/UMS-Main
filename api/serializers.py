ea477982f4e1ec7f612be8c547d6a21d5d69610c
Elijah Lopez <Elijah.kane.1972@gmail.com>
2022-09-02 16:26:34 -1000

    Initial Commit

--- api/serializers.py
+++ api/serializers.py
@@ -7,26 +7,3 @@ from courses.models import Course, CourseTime
 from homework.models import HomeworkAssignment
 
 
-class CourseRelatedField(serializers.RelatedField):
-    def to_representation(self, instance):
-        return model_to_dict(instance)
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        exclude = []
-
-
-class AssignmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HomeworkAssignment
-        exclude = []
-
-
-class CoursetimeSerializer(serializers.ModelSerializer):
-    course = CourseRelatedField(read_only=True)
-
-    class Meta:
-        model = CourseTime
-        exclude = []
